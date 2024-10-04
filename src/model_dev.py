52cebcdec883562027ceb4f282f2b84af30d83fc
GuillermoSainz07 <86427154+GuillermoSainz07@users.noreply.github.com>
2024-10-03 17:30:10 -0700

    update

--- src/model_dev.py
+++ src/model_dev.py
@@ -1,27 +1,78 @@
 import mlflow
 from abc import ABC, abstractmethod
 
+from darts.models.forecasting.baselines import NaiveDrift
+
 class Model(ABC):
+
+    def __init__(self,
+                 model_name:str,
+                 run_id:str,
+                 name_experiment_intance:str):
+        
+        """
+        Construct Method
+        Args:
+            model_name: Name to register model
+            run_id: Runs ID of experiment
+            name_experiment_intance: Models Name of experiment instance
+        """
+        self.model_name = model_name
+        self.client = mlflow.MlflowClient()
+        self.model_uri = f"runs:/{run_id}/{name_experiment_intance}"
+        self.name_experiment_intance = name_experiment_intance
+
+    @abstractmethod
+    def track_model(self):
+        pass
+
     @abstractmethod
     def train(self, X, y):
         pass
 
 
-class ExampleModel(Model):
+class BaseLineModel(Model):
     def __init__(self,
-                 register_id:str=None,
-                 model_name:str=None):
-        """
-        Construct Method
-        Args:
-            register_id: Register ID Models in MLFlow
-        """
-        self.register_id = register_id
+                 model_name:str,
+                 run_id:str,
+                 name_experiment_intance:str):
+        super().__init__(model_name=model_name,
+                         run_id=run_id,
+                         name_experiment_intance=name_experiment_intance)
+        
+    def track_model(self):
+        mlflow.register_model(self.model_uri)
+        latest_mv = self.client.get_lastest_versions(self.model_name)[0]
+        self.client.set_registered_model_alias(self.model_name, "staging", latest_mv.version)
+        self.client.set_registered_model_tag(self.model_name,"Developer","Guillermo")
+        self.client.set_model_version_tag(self.model_name, latest_mv.version,"validation_status")
 
-    def train(self):
-        with mlflow.start_run() as run:
+        self.client.update_model_version(name=self.model_name,
+                                         version=latest_mv.version,
+                                         description="Description")
+        
+        self.client.update_registered_model(name=self.model_name,
+                                            description="Description")
+    def train(self,
+              train,
+              val,
+              test):
+        #mlflow.set_experiment('')
 
-            client = mlflow.MlflowClient()
-            client.transition_model_version_stage(stage='production')
+        with mlflow.start_run(run_name=f'Retrain {self.model_name}') as run:
+            model = NaiveDrift()
+
+            mlflow.darts.autolog()
+
+            mlflow.darts.log_model(model, self.name_experiment_intance)
+            model_uri = f"runs:/{run.info.run_id}/{self.name_experiment_intance}"
+
+            model.register_model(model_uri, self.model_name)
+
+
+
+
+        
+        
 
 
